0ab2de5c8197de44e493626c8e1e28dfce2bd17b
nileshrn01 <146316325+r-nilesh-c@users.noreply.github.com>
2025-07-08 16:26:10 +0000

    deploy 2

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/Career-Counsel/',
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  }
+})
